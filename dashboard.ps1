b5c1b6f781aee2a3fe3ac8e388e9a217ef44a61e
Tony Skidmore <tony@skidmore.co.uk>
2019-02-18 01:03:07 +0000

    setting endpoint

--- dashboard.ps1
+++ dashboard.ps1
@@ -1,4 +1,27 @@
-$MyDashboard = New-UDDashboard -Title "Hello World" -Content {
-    New-UDCard -Title "Hello World - Universal Dashboard" 
+
+
+$Schedule = New-UDEndpointSchedule -Every 1 -Minute
+
+$EveryHour = New-UDEndpoint -Schedule $Schedule -Endpoint {
+    $Cache:ModuleCount = (Get-Module -ListAvailable).Count
 }
-Start-UDDashboard -Port 8585 -Dashboard $MyDashboard -Name 'HelloWorldDashboard' -Wait
+
+$moduleCount = New-UDEndpoint -Url "/moduleCount" -Endpoint {
+    (Get-Module -ListAvailable).Count
+}
+
+$CachedModuleCount = New-UDEndpoint -Url "/cached-moduleCount" -Endpoint {
+    $Cache:ModuleCount
+}
+
+$dashboard = New-UDDashboard -Title "Test" -Content {
+    New-UDCounter -Title "Test" -Id "Counter" -Endpoint {
+        $Cache:ModuleCount
+    } -AutoRefresh -RefreshInterval 1
+}
+
+$MyDashboard = New-UDDashboard -Title "Hello World" -Content {
+    New-UDCard -Title "Say Hello to ALOHA"
+}
+
+Start-UDDashboard -Port 8585 -Dashboard $dashboard -Name 'ALOHA' -Wait -Endpoint @($EveryHour, $ModuleCount, $CachedModuleCount)
