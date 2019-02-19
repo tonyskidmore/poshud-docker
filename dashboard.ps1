62cde3ede002860fe745de096ef5ca06dce72bc7
Tony Skidmore <tony@skidmore.co.uk>
2019-02-19 03:14:43 +0000

    using example

--- dashboard.ps1
+++ dashboard.ps1
@@ -1,18 +1,24 @@
+<#
+    Provies an example of creating a chart with multiple datasets.
+#>
 
+Import-Module UniversalDashboard.Community
 
-$Schedule = New-UDEndpointSchedule -Every 1 -Minute
-
-$EveryMinute = New-UDEndpoint -Schedule $Schedule -Endpoint {
-    $Data = Import-Csv -Path "/var/alohadata/abc-runner1-config-reports.csv"
-}
+$Data = @(
+    @{Animal="Frog";Count=10;Babies=3}
+    @{Animal="Tiger";Count=1;Babies=0}
+    @{Animal="Bat";Count=34;Babies=30}
+    @{Animal="Fox";Count=20;Babies=10}
+)
 
 $Dashboard = New-UDDashboard -Title "Charts - Multiple Datasets" -Content {
     New-UDChart -Title "Bottom Legend" -Type "Bar" -Endpoint {
-        $Data | Out-UDChartData -LabelProperty "ComputerName"  -Dataset @(
-            New-UDBarChartDataset -Label "Pass" -DataProperty RegStandardPass -BackgroundColor "#205D4CFF" -BorderColor "#5D4CFF" -BorderWidth 3
-            New-UDBarChartDataset -Label "Fail" -DataProperty RegStandardFail -BackgroundColor "#20A07DFF" -BorderColor "#A07DFF" -BorderWidth 3
+        $Data | Out-UDChartData -LabelProperty "Animal"  -Dataset @(
+            New-UDBarChartDataset -Label "Adults" -DataProperty Count -BackgroundColor "#205D4CFF" -BorderColor "#5D4CFF" -BorderWidth 3
+            New-UDBarChartDataset -Label "Babies" -DataProperty Babies -BackgroundColor "#20A07DFF" -BorderColor "#A07DFF" -BorderWidth 3
         )
     } 
 }
 
-Start-UDDashboard -Port 8585 -Dashboard $Dashboard -Name 'ALOHA' -Wait -Endpoint @($EveryMinute)
+
+Start-UDDashboard -Port 8585 -Dashboard $Dashboard -Name 'ALOHA' -Wait
