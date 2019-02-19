060a64a93fbdfcc8189dbb2e34b1941cf6e4ed00
Tony Skidmore <tony@skidmore.co.uk>
2019-02-19 02:56:01 +0000

    changing format

--- dashboard.ps1
+++ dashboard.ps1
@@ -1,11 +1,13 @@
 
-
+<#
 $Schedule = New-UDEndpointSchedule -Every 1 -Minute
 
 $EveryMinute = New-UDEndpoint -Schedule $Schedule -Endpoint {
     $Data = Import-Csv -Path "/var/alohadata/abc-runner1-config-reports.csv"
 }
+#>
 
+$Data = Import-Csv -Path "/var/alohadata/abc-runner1-config-reports.csv"
 
 $Dashboard = New-UDDashboard -Title "Charts - Legend" -Content {
 
@@ -13,10 +15,11 @@ $Dashboard = New-UDDashboard -Title "Charts - Legend" -Content {
     $Options = New-UDLineChartOptions -LegendOptions $LegendOptions
 
     New-UDChart -Title "Bottom Legend" -Type "Line" -Endpoint {
-        $Data | Out-UDChartData -LabelProperty "ALOHA"  -Dataset @(
+        $Data | Out-UDChartData -LabelProperty "ComputerName"  -Dataset @(
             New-UDLineChartDataset -Label "ALOHA Data" -DataProperty RegStandardFail -BackgroundColor "#205D4CFF" -BorderColor "#5D4CFF" -BorderWidth 3
         )
     } -Options $Options
 }
 
-Start-UDDashboard -Port 8585 -Dashboard $Dashboard -Name 'ALOHA' -Wait -Endpoint @($EveryMinute)
+# Start-UDDashboard -Port 8585 -Dashboard $Dashboard -Name 'ALOHA' -Wait -Endpoint @($EveryMinute)
+Start-UDDashboard -Port 8585 -Dashboard $Dashboard -Name 'ALOHA'
--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ docker run \
 -d \
 -p 80:8585 \
 --name ud \
+--mount type=bind,source=/var/alohadata,target=/var/alohadata \
 universaldashboard:latest
